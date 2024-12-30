be5586bd87239804c74eaf284eed4746cc84d8b7
Giu27 <Caruso.giuseppe09@hotmail.com>
2024-12-30 13:58:18 +0100

    added an fps counter

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ import sys
 import pygame as pg
 
 pg.init()
-pg.display.init()
+pg.font.init()
 debug = False
 
 screen = pg.display.set_mode((WIDTH,HEIGHT),0,32)
 pg.display.set_caption("Self Driving Car")
 clock = pg.time.Clock()
 
+font = pg.font.SysFont(None,FONT_SIZE)
+
 cars = pg.sprite.Group()
-cars.add(Car("Player",(WIDTH //2,HEIGHT//2),0,True),Car("AI",(220,300),227,True))
+cars.add(Car("Player",(WIDTH //2,HEIGHT//2),0,True))
 
 while True:
     for event in pg.event.get():
@@ -26,9 +28,12 @@ while True:
     screen.fill("white")
     cars.update()
     cars.draw(screen)
-    if debug:
+    if debug and DEBUG:
         for sprite in cars:
             pg.draw.rect(screen,"red",sprite.rect,2)
             screen.blit(sprite.mask.to_surface(setcolor="purple",unsetcolor=None),sprite.rect)
+        fps = int(clock.get_fps())
+        fps_surf = font.render(f"FPS: {fps}",True,"black")
+        screen.blit(fps_surf,(WIDTH - 3 * FONT_SIZE,FONT_SIZE))
     pg.display.update()
     clock.tick(FPS)
