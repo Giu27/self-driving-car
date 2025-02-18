33d00c3c43cc065546740f6c789f7cbfe4756645
Giu27 <Caruso.giuseppe09@hotmail.com>
2025-02-18 21:47:07 +0100

    discarded the code of the road file and used a much easier approach until I learn procedural generation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from settings import *
 from car import Car
-from road import Point, Segment
+from road import Track
 import sys
 import pygame as pg
 
@@ -15,12 +15,9 @@ clock = pg.time.Clock()
 font = pg.font.SysFont(None,FONT_SIZE)
 
 cars = pg.sprite.Group()
-cars.add(Car("Player",(WIDTH //2,HEIGHT//2),0,True))
+cars.add(Car("Player",(WIDTH //2,710 ),0,True))
 
-points = pg.sprite.Group()
-points.add(Point((100,100),RADIUS),Point((200,200),RADIUS),Point((300,300),RADIUS))
-segments = pg.sprite.Group()
-segments.add(Segment((points.sprites()[0],points.sprites()[1]),SEG_WIDTH),Segment((points.sprites()[1],points.sprites()[2]),SEG_WIDTH))
+track = pg.sprite.GroupSingle(Track("assets/track.png"))
 
 while True:
     for event in pg.event.get():
@@ -32,20 +29,14 @@ while True:
                 debug = not debug 
 
     screen.fill("white")
-    points.draw(screen)
-    segments.draw(screen)
-    cars.update((points,segments))
+    track.draw(screen)
+    cars.update((track,))
     cars.draw(screen)
     if debug and DEBUG:
         for sprite in cars:
             pg.draw.rect(screen,"red",sprite.rect,2)
             screen.blit(sprite.mask.to_surface(setcolor="purple",unsetcolor=None),sprite.rect)
-        for sprite in points:
-            pg.draw.rect(screen,"red",sprite.rect,2)
-            screen.blit(sprite.mask.to_surface(setcolor="purple",unsetcolor=None),sprite.rect)
-        for sprite in segments:
-            pg.draw.rect(screen,"red",sprite.rect,2)
-            screen.blit(sprite.mask.to_surface(setcolor="purple",unsetcolor=None),sprite.rect)
+        screen.blit(track.sprite.mask.to_surface(setcolor="purple",unsetcolor=None),track.sprite.rect)
         fps = int(clock.get_fps())
         fps_surf = font.render(f"FPS: {fps}",True,"black")
         screen.blit(fps_surf,(WIDTH - 3 * FONT_SIZE,FONT_SIZE))
--- road.py
+++ road.py
@@ -1,29 +1,11 @@
 import pygame as pg
+import os
 from settings import *
-import math
 
-class Point(pg.sprite.Sprite):
-    def __init__(self,pos,radius):
+class Track(pg.sprite.Sprite):
+    def __init__(self,image):
         super().__init__()
-        self.x = pos[0]
-        self.y = pos[1]
-        self.radius = radius
-        self.image = pg.Surface((radius * 2,radius *2),pg.SRCALPHA)
-        pg.draw.circle(self.image,POINT_COLOR,(radius,radius),radius)
-        self.rect = self.image.get_frect(center=(self.x,self.y))
-        self.mask = pg.mask.from_surface(self.image)
-
-class Segment(pg.sprite.Sprite):
-    def __init__(self,points,width):
-        super().__init__()
-        self.points = points
-        self.width = width
-        self.dx = self.points[1].x - self.points[0].x
-        self.dy = self.points[1].y - self.points[0].y
-        self.lenght = math.sqrt(self.dx**2 + self.dy**2)
-        self.angle = math.degrees(math.atan2(self.dy,self.dx))
-        self.image = pg.Surface((self.lenght,self.width),pg.SRCALPHA)
-        pg.draw.line(self.image,SEGMENT_COLOR,(0,self.width // 2),(self.lenght,self.width//2),self.width)
-        self.image = pg.transform.rotate(self.image,-self.angle)
-        self.rect = self.image.get_frect(center=((self.points[1].x + self.points[0].x)/2,(self.points[1].y + self.points[0].y)/2))
+        self.image = pg.image.load(os.path.join(image)).convert_alpha()
+        self.image.set_colorkey("white",pg.SRCALPHA)
+        self.rect = self.image.get_frect(topleft=(0,0))
         self.mask = pg.mask.from_surface(self.image)
